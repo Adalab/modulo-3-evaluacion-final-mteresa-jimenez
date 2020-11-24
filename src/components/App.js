0c5ea758f0925c1499d8eba894a3f645f77ba568
mteresa-jimenez <mteresa.jn@gmail.com>
2020-11-24 20:55:21 +0100

    Add filter

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import { useState, useEffect } from "react";
 import CharacterList from "./CharacterList";
 
 const App = (props) => {
+  // state
   const [characters, setCharacters] = useState([]);
+  const [filterText, setFilterText] = useState("");
 
+  // api
   useEffect(() => {
     api.getDataFromApi().then((data) => {
       setCharacters(data);
@@ -12,13 +15,24 @@ const App = (props) => {
     });
   }, []);
 
+  //events
+
   const handleFilter = (filterText) => {
-    console.log("ha cambiado", filterText);
+    return setFilterText(filterText);
   };
+
+  const filteredCharacters = characters.filter((character) => {
+    return character.name.toUpperCase().includes(filterText.toUpperCase());
+  });
+
+  console.log(filteredCharacters);
 
   return (
     <div className="App">
-      <CharacterList characters={characters} handleFilter={handleFilter} />
+      <CharacterList
+        characters={filteredCharacters}
+        handleFilter={handleFilter}
+      />
     </div>
   );
 };
